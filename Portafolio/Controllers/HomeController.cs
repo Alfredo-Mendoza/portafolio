4f348ef96bb8d24cd0b876dbfa59c3f5b8add420
Alfredo Mendoza <alfredomendozamg@gmail.com>
2025-03-01 20:20:34 -0600

    feat: add section proyects

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Portafolio.Models;
+using Portafolio.Models.DTOs;
 
 namespace Portafolio.Controllers
 {
@@ -15,13 +16,39 @@ namespace Portafolio.Controllers
 
         public IActionResult Index()
         {
-            Persona persona = new Persona()
-            {
-                Nombre = "Alfredo Mendoza",
-                Edad = 30
+            List<ProyectoDTO> proyectos = GetProyects().Take(3).ToList();
+            var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
+            return View(modelo);
+        }
+
+        private List<ProyectoDTO> GetProyects()
+        {
+            return new List<ProyectoDTO>() { 
+                new ProyectoDTO {
+                    Titulo = "Amazon",
+                    Descripcion = "E-Commerce realizado en ASP.Net Core",
+                    Link = "https://www.amazon.com.mx",
+                    ImagenURL = "/imagenes/amazon.png"
+                },
+                new ProyectoDTO {
+                    Titulo = "New York Times",
+                    Descripcion = "Páginas de noticias en React",
+                    Link = "https://www.nytimes.com",
+                    ImagenURL = "/imagenes/nyt.png"
+                },
+                new ProyectoDTO {
+                    Titulo = "Reddit",
+                    Descripcion = "Red social para compartir en comunidades",
+                    Link = "https://www.reddit.com",
+                    ImagenURL = "/imagenes/reddit.png"
+                },
+                new ProyectoDTO {
+                    Titulo = "Steam",
+                    Descripcion = "Tienda en línea para comprar vídeojeugos",
+                    Link = "www.steam.com",
+                    ImagenURL = "/imagenes/steam.png"
+                }
             };
-            
-            return View(persona);
         }
 
         public IActionResult Privacy()
---
+++ Portafolio/Models/HomeIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Portafolio.Models.DTOs;
+
+namespace Portafolio.Models
+{
+    public class HomeIndexViewModel
+    {
+        public IEnumerable<ProyectoDTO> Proyectos { get; set; }
+    }
+}
