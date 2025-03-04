10a6c69181d11a0d5317d71a80495bc0bae90415
Alfredo Mendoza <alfredomendozamg@gmail.com>
2025-03-03 22:46:33 -0600

    feat: add projects as a service

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -2,53 +2,26 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Portafolio.Models;
 using Portafolio.Models.DTOs;
+using Portafolio.Services;
 
 namespace Portafolio.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IRepositoryProjects repositoryProjects;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IRepositoryProjects repositoryProjects)
         {
             _logger = logger;
+            this.repositoryProjects = repositoryProjects;
         }
 
         public IActionResult Index()
         {
-            List<ProyectoDTO> proyectos = GetProyects().Take(3).ToList();
+            List<ProyectoDTO> proyectos = repositoryProjects.GetProyects().Take(3).ToList();
             var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
             return View(modelo);
-        }
-
-        private List<ProyectoDTO> GetProyects()
-        {
-            return new List<ProyectoDTO>() { 
-                new ProyectoDTO {
-                    Titulo = "Amazon",
-                    Descripcion = "E-Commerce realizado en ASP.Net Core",
-                    Link = "https://www.amazon.com.mx",
-                    ImagenURL = "/imagenes/amazon.png"
-                },
-                new ProyectoDTO {
-                    Titulo = "New York Times",
-                    Descripcion = "Páginas de noticias en React",
-                    Link = "https://www.nytimes.com",
-                    ImagenURL = "/imagenes/nyt.png"
-                },
-                new ProyectoDTO {
-                    Titulo = "Reddit",
-                    Descripcion = "Red social para compartir en comunidades",
-                    Link = "https://www.reddit.com",
-                    ImagenURL = "/imagenes/reddit.png"
-                },
-                new ProyectoDTO {
-                    Titulo = "Steam",
-                    Descripcion = "Tienda en línea para comprar vídeojeugos",
-                    Link = "www.steam.com",
-                    ImagenURL = "/imagenes/steam.png"
-                }
-            };
         }
 
         public IActionResult Privacy()
---
+++ Portafolio/Services/RepositoryProjects.cs
@@ -0,0 +1,42 @@
+﻿using Portafolio.Models.DTOs;
+
+namespace Portafolio.Services
+{
+    public interface IRepositoryProjects
+    {
+        List<ProyectoDTO> GetProyects();
+    }
+
+    public class RepositoryProjects: IRepositoryProjects
+    {
+        public List<ProyectoDTO> GetProyects()
+        {
+            return new List<ProyectoDTO>() {
+                new ProyectoDTO {
+                    Titulo = "Amazon",
+                    Descripcion = "E-Commerce realizado en ASP.Net Core",
+                    Link = "https://www.amazon.com.mx",
+                    ImagenURL = "/imagenes/amazon.png"
+                },
+                new ProyectoDTO {
+                    Titulo = "New York Times",
+                    Descripcion = "Páginas de noticias en React",
+                    Link = "https://www.nytimes.com",
+                    ImagenURL = "/imagenes/nyt.png"
+                },
+                new ProyectoDTO {
+                    Titulo = "Reddit",
+                    Descripcion = "Red social para compartir en comunidades",
+                    Link = "https://www.reddit.com",
+                    ImagenURL = "/imagenes/reddit.png"
+                },
+                new ProyectoDTO {
+                    Titulo = "Steam",
+                    Descripcion = "Tienda en línea para comprar vídeojeugos",
+                    Link = "www.steam.com",
+                    ImagenURL = "/imagenes/steam.png"
+                }
+            };
+        }
+    }
+}
